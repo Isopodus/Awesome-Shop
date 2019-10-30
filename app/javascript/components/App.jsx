f5ef5a24cc7456d9c567784a98b3bc30fbbec48d
Isopodus <isopodus@gmail.com>
2019-10-30 21:13:57 +0200

    Added react router and Homepage component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
 
 function App() {
     return (
         <div>
-            Hello world!
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
         </div>
     )
 }
---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+function Home() {
+    return (
+        <div>
+            This is my homepage
+        </div>
+    )
+}
+
+export default Home
